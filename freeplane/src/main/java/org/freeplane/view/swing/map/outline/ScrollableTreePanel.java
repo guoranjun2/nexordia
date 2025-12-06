99fe38696342ec38a9b8dd23f46a7abd0553adb8
Dimitry Polivaev <dpolivaev@gmx.de>
2025-12-06 20:20:28 +0100

    remove unused import

--- freeplane/src/main/java/org/freeplane/view/swing/map/outline/ScrollableTreePanel.java
+++ freeplane/src/main/java/org/freeplane/view/swing/map/outline/ScrollableTreePanel.java
@@ -2,7 +2,6 @@ package org.freeplane.view.swing.map.outline;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.KeyboardFocusManager;
 import java.awt.event.MouseAdapter;
